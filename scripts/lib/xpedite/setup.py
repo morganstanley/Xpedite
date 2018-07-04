5d44459482ef4744962cacccbacb6955ce15f0db
Manikandan Dhamodharan <Mani-D@users.noreply.github.com>
2018-07-04 02:21:46 -0400

    Updated long description, license and dependencies in setup.py

--- scripts/lib/xpedite/setup.py
+++ scripts/lib/xpedite/setup.py
@@ -8,10 +8,18 @@ from setuptools import setup
 
 setup(name='xpedite',
       version='1.0',
-      description='Xpedite is a probe based profiler build to optimize performance of ultra-low-latency applications',
+      description='Xpedite is a probe based profiler used to optimize performance of ultra-low-latency applications',
       long_description=(
-        'Xpedite is a probe based profiler build to optimize performance of '
-        'ultra-low-latency applications'
+        """
+        A probe based profiler used to, measure and optimise, performance of ultra-low-latency / real time systems.
+
+        The main features include
+          
+          1. Quantify how efficiently "a software stack" or "a section of code", is running in a target platform (CPU/OS).
+          2. Do Cycle accounting and bottleneck analysis using hardware performance counters and top-down micro architecture analysis methodology
+          3. Filter, query and visualise performance statistics with real time interactive shell.
+          4. Prevent regressions, by benchmarking latency statistics for multiple runs/builds side-by-side. 
+        """
       ),
       classifiers=[
         'Programming Language :: Python :: 2.7',
@@ -21,7 +29,7 @@ setup(name='xpedite',
       url='https://github.com/Morgan-Stanley/Xpedite',
       author='Manikandan Dhamodharan',
       author_email='Mani-D@users.noreply.github.com',
-      license='MIT',
+      license='Apache 2.0',
       packages=['xpedite'],
       install_requires=[
         'enum34',
@@ -35,13 +43,7 @@ setup(name='xpedite',
         'cement',
         'termcolor',
         'py-cpuinfo',
-        'ipython_genutils',
-        'jsonschema',
-        'nbformat',
-        'notebook',
-        'six',
-        'tornado',
-        'traitlets'
+        'jupyter',
       ],
       include_package_data=True,
       zip_safe=False)
