3b0f7f6b5dda36b97588cb0574d96b36d97734cd
Brooke Cantwell <becantwell@users.noreply.github.com>
2019-09-05 15:33:38 -0400

    Changes to Java CallSites

--- jni/src/main/java/com/xpedite/probes/AnchoredProbe.java
+++ jni/src/main/java/com/xpedite/probes/AnchoredProbe.java
@@ -10,23 +10,11 @@ package com.xpedite.probes;
 
 public class AnchoredProbe extends AbstractProbe {
 
-    private int lineNo;
     private CallSite[] callSites;
-
-    private String buildProbeName(String className, String methodName, int lineNo) {
-        StringBuffer buffer = new StringBuffer(className);
-        buffer.append(".").append(methodName);
-        buffer.append(":").append(lineNo);
-        return buffer.toString();
-    }
 
     public AnchoredProbe(String className, String methodName, int lineNo) {
         super(className, methodName);
-        callSites = new CallSite[] { new CallSite(this, buildProbeName(className, methodName, lineNo))};
-    }
-
-    public int getLineNo() {
-        return this.lineNo;
+        callSites = new CallSite[] { new CallSite(className, methodName, lineNo)};
     }
 
     @Override
